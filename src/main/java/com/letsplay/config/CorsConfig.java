92ba1cc64a76f8fb6ae4c9821d147a9ca47e72d5
f <medlfarssi10@gmail.com>
2026-02-10 23:54:02 +0100

    config cors

--- src/main/java/com/letsplay/config/CorsConfig.java
+++ src/main/java/com/letsplay/config/CorsConfig.java
@@ -1,5 +1,25 @@
 package com.letsplay.config;
 
+import org.springframework.context.annotation.*;
+import org.springframework.web.cors.*;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
 public class CorsConfig {
-    
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration cfg = new CorsConfiguration();
+        cfg.setAllowedOrigins(List.of("http://localhost:3000"));
+        cfg.setAllowedMethods(List.of("GET","POST","PUT","DELETE"));
+        cfg.setAllowedHeaders(List.of("*"));
+        cfg.setAllowCredentials(true);
+
+        org.springframework.web.cors.UrlBasedCorsConfigurationSource source =
+                new org.springframework.web.cors.UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", cfg);
+        return source;
+    }
 }
